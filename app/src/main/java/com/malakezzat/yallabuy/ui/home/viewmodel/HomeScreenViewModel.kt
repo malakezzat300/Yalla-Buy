7f9a684e3de313094f131d8a37295ad534f2795a
Hager Abobakr <hagarabobakr1@gmail.com>
2024-10-03 20:42:27 +0300

    Navigation

---
+++ app/src/main/java/com/malakezzat/yallabuy/ui/home/viewmodel/HomeScreenViewModel.kt
@@ -0,0 +1,9 @@
+package com.malakezzat.yallabuy.ui.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.malakezzat.yallabuy.data.ProductsRepository
+
+class HomeScreenViewModel(private val repository: ProductsRepository):ViewModel() {
+
+
+}
