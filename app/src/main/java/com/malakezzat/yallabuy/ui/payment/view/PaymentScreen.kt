af84929164f6354e145b323881692a3c2ca5acc1
malakezzat300 <47084913+malakezzat300@users.noreply.github.com>
2024-10-04 23:22:24 +0300

    OrderScreen - UI

--- app/src/main/java/com/malakezzat/yallabuy/ui/payment/view/PaymentScreen.kt
+++ app/src/main/java/com/malakezzat/yallabuy/ui/payment/view/PaymentScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.malakezzat.yallabuy.ui.theme.YallaBuyTheme
-import com.ramcosta.composedestinations.annotation.Destination
 
 @Composable
 fun PaymentScreen(onPaymentSuccess: () -> Unit) {
