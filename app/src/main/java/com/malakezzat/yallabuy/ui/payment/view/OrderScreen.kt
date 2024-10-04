8d062846c0b55c3b93a9cf26c98005ec6aebe919
malakezzat300 <47084913+malakezzat300@users.noreply.github.com>
2024-10-04 23:28:39 +0300

    checkout screen UI

--- app/src/main/java/com/malakezzat/yallabuy/ui/payment/view/OrderScreen.kt
+++ app/src/main/java/com/malakezzat/yallabuy/ui/payment/view/OrderScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +21,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.malakezzat.paymenttest2.model.FakeData
+import com.malakezzat.yallabuy.ui.payment.viewmodel.PaymentViewModel
 
 
 @Composable
