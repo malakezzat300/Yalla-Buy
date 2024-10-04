bae6e75172bc7cfaebd8354225a679bcd9b2aabc
malakezzat300 <47084913+malakezzat300@users.noreply.github.com>
2024-10-05 02:45:09 +0300

    Shopping Cart

---
+++ app/src/main/java/com/malakezzat/yallabuy/ui/shoppingcart/view/ShoppingCartScreen.kt
@@ -0,0 +1,11 @@
+package com.malakezzat.yallabuy.ui.shoppingcart.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ShoppingCartScreen() {
+
+
+}
