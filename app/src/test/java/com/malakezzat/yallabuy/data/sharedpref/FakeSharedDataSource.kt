ebb5ebb1e50e7d866599e50688bd5ec59c2dbc6b
Hager Abobakr <hagarabobakr1@gmail.com>
2024-10-11 17:18:12 +0300

    testing step1

---
+++ app/src/test/java/com/malakezzat/yallabuy/data/sharedpref/FakeSharedDataSource.kt
@@ -0,0 +1,19 @@
+package com.malakezzat.yallabuy.data.sharedpref
+
+class FakeSharedDataSource:GlobalSharedPreferenceDataSource {
+    override fun getUserId(): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun setUserId(id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserEmail(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun setUserEmail(string: String) {
+        TODO("Not yet implemented")
+    }
+}
