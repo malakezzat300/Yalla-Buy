8e2505be9d734cbc58f3b6d7d8890de98331e399
Hager Abobakr <hagarabobakr1@gmail.com>
2024-10-11 22:21:16 +0300

    testing repo

--- app/src/test/java/com/malakezzat/yallabuy/data/ProductsRepositoryImplTest.kt
+++ app/src/test/java/com/malakezzat/yallabuy/data/ProductsRepositoryImplTest.kt
@@ -1,6 +1,38 @@
 package com.malakezzat.yallabuy.data
 
+import com.malakezzat.yallabuy.data.remot.FakeRemoteDataSource
+import com.malakezzat.yallabuy.data.sharedpref.FakeSharedDataSource
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Before
+import org.junit.Test
+
 
 class ProductsRepositoryImplTest{
+    private val fakeRemoteDataSource =  FakeRemoteDataSource()
+    private val fakeSharedDataSource = FakeSharedDataSource()
+    //private val ProductsRepositoryImpl  = ProductsRepositoryImpl()
+    private lateinit var productsRepositoryImpl: ProductsRepository
+    @Before
+    fun setUp() {
+        productsRepositoryImpl = ProductsRepositoryImpl.getInstance(fakeRemoteDataSource,
+            fakeSharedDataSource)
+    }
+
+    @Test
+    fun getAllProducts_return_fake_list() = runTest {
+        val result = productsRepositoryImpl.getAllProducts()
+        result.collect{ prductList->
+            assertThat(prductList.size,`is`(3))
+        }
+    }
+   @Test
+   fun getProductsByCollectionId_return_fake_list()= runTest {
+       val result = productsRepositoryImpl.getProductsByCollectionId(1)
+       result.collect {prduct->
+           assertThat(prduct.get(0).id,`is`(1) )
+       }
+   }
 
 }
